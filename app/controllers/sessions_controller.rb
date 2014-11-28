bf64249624569886b6fff03479127dbdffda5bd9
Timothy Howard <iamtimhoward@users.noreply.github.com>
2014-11-28 11:43:41 -0500

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,26 @@
 class SessionsController < ApplicationController
-	def new
-		if current_user
-			redirect_to(root_path)
-		else
-			render 'new'
-		end
-	end
+	
+  def new
+    if current_user
+     redirect_to(root_path)
+    else
+     render 'new'
+    end
+  end
 
-	def create
-		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
-			session[:current_user_id] = user.id
-			redirect_to root_path
-		else
-			redirect_to root_path
-		end
-	end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:current_user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
-	def destroy
-		session.clear
-		redirect_to root_path
-	end
-
+  def destroy
+   session.clear
+   redirect_to root_path
+  end
+  
 end
